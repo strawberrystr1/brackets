c5d6d91b091cf2db14f809ef86dc78f4b97b39bf
strawberrystr1 <andrey_overdose@mail.ru>
2021-03-08 11:40:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ for (let i = str.length; i >= 0; i--) {
+    for (let j=0; j< bracketsConfig.length; j++) {
+      let bracket = bracketsConfig[j].join('')
+      if (str.includes(bracket)) {
+        str = str.replace(bracket, '')
+      }
+    }
+  }
+  return str === '' ? true: false;
 }
+
+
+
+
